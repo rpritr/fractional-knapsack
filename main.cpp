c5cbc9611e6eecdcfe8ce5fbaaab259aaac49377
rpritr <robi@foreach-labs.si>
2025-06-06 21:59:13 +0200

    0/1 fractional knapsack problem

--- main.cpp
+++ main.cpp
@@ -7,67 +7,87 @@ using namespace std;
 class Knapsack
 {
 private:
-    vector<pair<int, int>> items;
-    int V;
-    vector<pair<int, int>> o;
-    int resCapacity;
+    vector<pair<double, double>> items;
+    double V;
+    vector<pair<double, double>> o;
+    double resValue;
+    double resCapacity;
 
 public:
-    Knapsack(int volume) { V = volume; }
-
-    Knapsack(vector<pair<int, int>> newItems, int volume)
+    Knapsack(double volume) { V = volume; }
+    Knapsack(vector<pair<double, double>> newItems, double volume)
     {
         this->items = newItems; // set items
         this->V = volume;
+        this->resValue = 0;
         this->resCapacity = 0;
     }
-    int getVolume() const { return V; }
-    vector<pair<int, int>> getOptimal() { return this->o; }
-    void setVolume(int volume) { V = volume; }
-    // void addItem(int newV) { v.push_back(newV); }
-    // void addPrice(int newP) { p.push_back(newP); }
+    double getVolume() const { return V; }
+    double getCapacity() const { return this->resCapacity; }
+    double getValue() const { return this->resValue; }
+
+    vector<pair<double, double>> getOptimal() { return this->o; }
+    void setVolume(double volume) { V = volume; }
     void sortItems()
     {
-        sort(items.begin(), items.end(), [](const pair<int, int> &a, const pair<int, int> &b)
+        sort(items.begin(), items.end(), [](const pair<double, double> &a, const pair<double, double> &b)
              {
             double r1 = (double)a.second / a.first;
             double r2 = (double)b.second / b.first;
             return r1 > r2; });
     }
-    int findGreedy()
+    void printItems()
+    {
+        for (pair<double, double> o : this->items)
+        {
+            cout << o.first << "-" << o.second << endl;
+        }
+    }
+    double findGreedy()
     {
         // sort items asc
         this->sortItems();
         // look for optimal solution
-        for (pair<int, int> item : this->items)
+        for (pair<double, double> item : this->items)
         {
             if (item.first <= this->V)
             {
                 o.push_back(item);
+                this->resValue += item.second;
                 this->resCapacity += item.first;
                 this->V -= item.first;
             }
             else
             {
                 // take fraction
+                this->resValue += item.second * this->V / item.first;
+                this->resCapacity += this->V;
+                o.push_back({this->V, item.second * this->V / item.first});
+                this->V = 0;
+                break;
             }
         }
-        return resCapacity;
+        return resValue;
     }
 };
 
 int main()
 {
-    std::cout << "Fractional 0/1 Knapsack" << std::endl;
+    cout << "Fractional 0/1 Knapsack" << endl;
 
-    vector<pair<int, int>> i = {{8, 8}, {5, 6}, {5, 6}};
+    // test items
+    vector<pair<double, double>> i = {{8, 8}, {5, 6}, {5, 6}};
+    vector<pair<double, double>> j = {{4, 4}, {6, 9}, {1, 2}, {2, 5}};
+    vector<pair<double, double>> k = {{6, 12}, {4, 8}, {5, 10}};
 
-    Knapsack t = Knapsack(i, 10);
-    cout << "Total Volume: " << t.getVolume() << endl;
-    int o = t.findGreedy();
-
-    cout << "Max Capacity: " << o << endl;
-    for (pair<int, int> o : t.getOptimal())
+    Knapsack t = Knapsack(k, 7);                       // create new knapsack with items and volume
+    cout << "Total Volume: " << t.getVolume() << endl; // print total volume
+    cout << "Items: " << endl;
+    t.printItems();                                     // print all items
+    double o = t.findGreedy();                          // find best optimal solution
+    cout << "Max. Volume: " << t.getCapacity() << endl; // print optimal volume
+    cout << "Max. Value: " << o << endl;                // print optimal value
+    for (pair<double, double> o : t.getOptimal())       // print optimal items
     {
         cout << o.first << "-" << o.second << endl;
     }
